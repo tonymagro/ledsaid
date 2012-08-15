3d4e73400dc279d508850a41bd27fd33f94176ce
Tony Magro <tony@twogear.com>
2012-08-15 14:58:24 -0500

    Add sign clean flag

--- main.go
+++ main.go
@@ -9,10 +9,11 @@ import (
 
 var t = flag.String("t", "", "Template")
 var a = flag.Bool("a", true, "Automatically allocate memory")
+var c = flag.Bool("c", false, "Clear the sign")
 
 func main() {
 	flag.Parse()
-	if *t == "" {
+	if *t == "" && *c == false {
 		return
 	}
 	usb.Init()
@@ -29,6 +30,13 @@ func main() {
 	}
 
 	sign.Reset()
+	if *c {
+		_, err = sign.WriteTemplate("{{Text .A}}{{.ETX}}")
+		if err != nil {
+			log.Println(err)
+		}
+		return
+	}
 	_, err = sign.WriteTemplate(*t)
 	if err != nil {
 		log.Println(err)
