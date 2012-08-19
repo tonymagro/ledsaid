b9b8dcc2e274f50893bcd4815237b423ab35941f
Tony Magro <tony@twogear.com>
2012-08-19 05:28:06 -0500

    Update code to use new asign template

--- main.go
+++ main.go
@@ -4,18 +4,25 @@ import (
 	"flag"
 	"github.com/Krussell/asign"
 	"github.com/Krussell/usb"
+	"io/ioutil"
 	"log"
+	"os"
 )
 
-var t = flag.String("t", "", "Template")
-var a = flag.Bool("a", true, "Automatically allocate memory")
-var c = flag.Bool("c", false, "Clear the sign")
+var (
+	t = flag.String("t", "", "Template")
+	a = flag.Bool("a", true, "Automatically allocate memory")
+	b = flag.Bool("b", false, "Blank the sign")
+	s = flag.Bool("s", false, "Read from stdin")
+	f = flag.String("f", "", "Read from file")
+)
 
 func main() {
 	flag.Parse()
-	if *t == "" && *c == false {
+	if *t == "" && *b == false && *s == false && *f == "" {
 		return
 	}
+
 	usb.Init()
 	signUSB, err := openSignUSB()
 	if err != nil {
@@ -30,16 +37,54 @@ func main() {
 	}
 
 	sign.Reset()
-	if *c {
-		_, err = sign.WriteTemplate("{{Text .A}}{{.ETX}}")
+	if *b {
+		sign.DisableAutoMemory = false
+		_, err = sign.WriteTemplate([]byte("{SOT}{STX}{WriteText}{A}{ETX}{EOT}"))
 		if err != nil {
 			log.Println(err)
 		}
 		return
 	}
-	_, err = sign.WriteTemplate(*t)
-	if err != nil {
-		log.Println(err)
-		return
+
+	if *t != "" {
+		_, err = sign.WriteTemplate([]byte(*t))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+
+	if *f != "" {
+		f, err := os.Open(*f)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer f.Close()
+
+		buf, err := ioutil.ReadAll(f)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		_, err = sign.WriteTemplate(buf)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+
+	if *s {
+		println("Reading from stdin")
+		buf, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		_, err = sign.WriteTemplate(buf)
+		if err != nil {
+			log.Println(err)
+			return
+		}
 	}
 }
