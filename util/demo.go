111d244fb51f6b22050ccaad8b382e2b52efedb6
Tony Magro <tony@twogear.com>
2012-08-19 14:54:21 -0500

    add simple mode demo

---
+++ util/demo.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Krussell/asign"
+)
+
+func main() {
+	fmt.Println("{SOT}")
+	fmt.Println("{STX}\n{WriteText}{A}")
+
+	for k := range asign.ModeCode {
+		if k == "Special" {
+			continue
+		}
+		fmt.Printf("{%s}%s\n", k, k)
+	}
+
+	fmt.Println("{ETX}")
+	fmt.Println("{EOT}")
+}
